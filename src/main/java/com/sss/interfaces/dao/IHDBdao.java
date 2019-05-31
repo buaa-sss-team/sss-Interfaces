97b627ad216b348828b72698e3d61b31add5467b
for0nething <245575413@qq.com>
2019-05-31 20:54:09 +0800

    完善IHDBdao的设计

--- src/main/java/com/sss/interfaces/dao/IHDBdao.java
+++ src/main/java/com/sss/interfaces/dao/IHDBdao.java
@@ -2,11 +2,13 @@ package com.sss.interfaces.dao;
 
 import com.sss.interfaces.hmodel.User;
 
+import java.util.List;
+
 public interface IHDBdao {
-    void init();
     void insertUser(User who);
     void updateUser(User who);
     User getUserByID(int id);
     void deleteUserByID(int ID);
+    List<User>getUserByName(String name);
 
 }
