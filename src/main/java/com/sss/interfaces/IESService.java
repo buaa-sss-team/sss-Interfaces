d504a467d07de268367be97e9506f12050b6e48f
zlccccc <940182901@qq.com>
2019-06-03 00:45:55 +0800

    test ES service

---
+++ src/main/java/com/sss/interfaces/IESService.java
@@ -0,0 +1,8 @@
+package com.sss.interfaces;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IESService {
+    public List<Map<String, Object>> FuzzyQueryAbstract(String info);
+}
