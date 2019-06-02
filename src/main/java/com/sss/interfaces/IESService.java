4a465e37bc7cf1e0654412b7a2dab8c2590d05bc
zlccccc <940182901@qq.com>
2019-06-03 01:20:48 +0800

    test ES service

--- src/main/java/com/sss/interfaces/IESService.java
+++ src/main/java/com/sss/interfaces/IESService.java
@@ -4,5 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public interface IESService {
-    public List<Map<String, Object>> FuzzyQueryAbstract(String info);
+    // 表名, 字段名, 模糊查询的值是几
+    // 返回的是所有字段形成的list_map, 先转json就提一下要求
+    // TODO: 目前的所有查询都是在paper表上, 明(今)天实现其他表
+    List<Map<String, Object>> FuzzyQueryAbstract(String name, String index, String info);
 }
