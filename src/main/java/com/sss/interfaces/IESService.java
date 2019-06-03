c175732424d51975f34c8d6b55b68f8c530491cd
zlccccc <940182901@qq.com>
2019-06-03 21:50:46 +0800

    test ES service

--- src/main/java/com/sss/interfaces/IESService.java
+++ src/main/java/com/sss/interfaces/IESService.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 public interface IESService {
-    // 表名, 字段名, 模糊查询的值是几
-    // 返回的是所有字段形成的list_map, 先转json就提一下要求
-    // TODO: 目前的所有查询都是在paper表上, 明(今)天实现其他表
-    // count是返回值的最多个数, 我定义是 <100 则 =100
+    /***
+     *
+     * @param name  表名, 只能是expert, paper, patent 三个之一
+     * @param index 字段名, 必须在表里
+     * @param info  查询的 information
+     * @param count 返回值的最多个数, 我定义是 <100 则 =100
+     * @return 所有字段形成的 map
+     */
     List<Map<String, Object>> FuzzyQueryAbstract(String name, String index, String info, int count);
 }
